fa8f47bceecb106d571880bfbfd5db0590b9a8e2
aatully <105517926+aaFranks@users.noreply.github.com>
2022-11-09 15:41:33 -0300

    Adicionando questões do livro CBB

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,9 @@
+class CBBAtividadePratica3 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+    }
+}
